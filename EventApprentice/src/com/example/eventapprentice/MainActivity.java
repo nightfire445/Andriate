e77422d8ed4485821b56f93c009e537c807e3519
Lee Saltzman <mccall.saltzman@gmail.com>
2014-03-15 18:18:59 -0400

    Added ability to import events from Facebook, and changed MainActivity to use fragments
    Added INTERNET permission to AndroidManifest
    Slightly improved initial screen, changed file to main.xml
    Added file list_item.xml for use in ListActivity
    
    TO DO:
    -fix storage of Atendees
    -display stored events
    -better error handling for failed HTTP REQUESTS

--- EventApprentice/src/com/example/eventapprentice/MainActivity.java
+++ EventApprentice/src/com/example/eventapprentice/MainActivity.java
@@ -1,20 +1,27 @@
 package com.example.eventapprentice;
 
 import com.example.eventapprentice.R;
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
+	private MainFragment mainFragment;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		if(savedInstanceState == null){
+			mainFragment = new MainFragment();
+			getSupportFragmentManager().beginTransaction().add(android.R.id.content, mainFragment).commit();
+		}else{
+			mainFragment = (MainFragment) getSupportFragmentManager().findFragmentById(android.R.id.content);
+		}        
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -24,6 +31,12 @@ public class MainActivity extends Activity {
 
 	public void newEvent(View view) {
 		Intent intent = new Intent(this, CreateNewEvent.class);
+		startActivity(intent);
+	}
+	
+	public void importFBEvent(View view){
+		
+		Intent intent = new Intent(this, ImportFBEvent.class);
 		startActivity(intent);
 	}
 	
